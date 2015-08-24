cf859fca8b65035eb52929c2408130016c63fbd8
Milosh <milos.dzepina@pstech.rs>
2015-08-24 18:14:41 +0200

    cleaning

--- src/actions/TodoActions.js
+++
@@ -1,4 +0,0 @@
-import AppDispatcher from '../dispatcher/AppDispatcher.js';
-import TodoActionCreatorFactory from './TodoActionCreatorFactory.js';
-
-export default TodoActionCreatorFactory.create(AppDispatcher);
