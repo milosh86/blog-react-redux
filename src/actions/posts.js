8feae3ec8494b71678d2da7251bb410c0c717180
Milosh <milos.dzepina@gmail.com>
2015-09-11 16:05:03 +0200

    actions

---
+++ src/actions/posts.js
@@ -0,0 +1,44 @@
+import types from '../constants/BlogConstants';
+
+export function createPost(post) {
+  return {
+    type: types.CREATE_POST,
+    post
+  }
+}
+
+export function updatePost(data) {
+  return {
+    type: types.UPDATE_POST,
+    data
+  }
+}
+
+export function deletePost(postId) {
+  return {
+    type: types.REMOVE_POST,
+    id: postId
+  }
+}
+
+export function createComment(comment) {
+  return {
+    type: types.CREATE_COMMENT,
+    comment
+  }
+}
+
+export function updateComment(data) {
+  return {
+    type: types.UPDATE_COMMENT,
+    data
+  }
+}
+
+export function deleteComment(comment) {
+  return {
+    type: types.REMOVE_COMMENT,
+    commentId: comment.commentId,
+    postId: comment.postId
+  }
+}
