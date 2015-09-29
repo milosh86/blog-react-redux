b4e35709a32fe1b79d09fb225faa0f0a2145649c
Milosh <milos.dzepina@gmail.com>
2015-09-29 10:44:55 +0200

    redbox fix

--- webpack/dev.client.config.js
+++ webpack/dev.client.config.js
@@ -24,15 +24,17 @@ try {
 babelLoaderQuery.plugins = ['react-transform'];
 babelLoaderQuery.extra = {
   'react-transform': {
-    'transforms': [{
-      'transform': 'react-transform-hmr',
-      'imports': ['react'],
-      'locals': ['module']
-    },
+    'transforms': [
+      {
+        'transform': 'react-transform-hmr',
+        'imports': ['react'],
+        'locals': ['module']
+      },
       {
         'transform': 'react-transform-catch-errors',
         'imports': ['react', 'redbox-react']
-      }]
+      }
+    ]
   }
 };
 
