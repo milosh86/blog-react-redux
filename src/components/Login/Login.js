90692480323701f106fdaf9a9c68e0182dd69e98
milosh86 <milos.dzepina@gmail.com>
2015-10-18 13:39:59 +0200

    Login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,23 @@
+import './Login.css';
+import React, {Component} from 'react';
+
+class Login extends Component {
+
+  static displayName = 'Login';
+
+  static propTypes = {
+    tmp: React.PropTypes.string.isRequired
+  };
+
+  static defaultProps = {
+    tmp: 'tmp'
+  };
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Login;
