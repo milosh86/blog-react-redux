02e0332ff57401ad712975acda4725d85105af1e
milosh86 <milos.dzepina@gmail.com>
2015-10-18 13:25:05 +0200

    adding Home route

--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,22 @@ import PostListContainer from './containers/PostListContainer/PostListContainer.
 import PostContainer from './containers/PostContainer/PostContainer.js';
 import NewPostContainer from './containers/NewPostContainer/NewPostContainer.js';
 import NotFound from './components/NotFound/NotFound.js';
+import Home from './components/Home/Home.js';
 
 export function getRoutes() {
   return (
-    <Route path='/blog' component={BlogContainer}>
-      <IndexRoute component={PostListContainer}/>
-      <Route path="post/:permalink" component={PostContainer}/>
-      <Route path="post(/)" component={NewPostContainer}/>
-      <Route path="newpost" component={NewPostContainer}/>
-      <Route path="tag/:tag" component={PostListContainer}/>
-      <Route path="archive/:month" component={PostListContainer}/>
-      <Route path="*" component={NotFound}/>
-    </Route>
+    <div>
+      <Route path='/' component={Home}/>
+      <Route path='/blog' component={BlogContainer}>
+        <IndexRoute component={PostListContainer}/>
+        <Route path="post/:permalink" component={PostContainer}/>
+        <Route path="post(/)" component={NewPostContainer}/>
+        <Route path="newpost" component={NewPostContainer}/>
+        <Route path="tag/:tag" component={PostListContainer}/>
+        <Route path="archive/:month" component={PostListContainer}/>
+        <Route path="*" component={NotFound}/>
+      </Route>
+    </div>
   );
 }
 
